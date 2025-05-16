d1e214c2cb847658d8036700fc97cd5ffd9a0ee1
Aarav Vashishtha <118906362+aaravsharma17@users.noreply.github.com>
2025-05-16 17:43:07 +0530

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+// Additional JavaScript can be added here
+document.addEventListener('DOMContentLoaded', function() {
+    // Close alert messages after 5 seconds
+    setTimeout(function() {
+        const alerts = document.querySelectorAll('.alert');
+        alerts.forEach(alert => {
+            const bsAlert = new bootstrap.Alert(alert);
+            bsAlert.close();
+        });
+    }, 5000);
+});
